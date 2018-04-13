3c700caa777caf420942658f88b70f024dfa799d
saurav <saurav@Robs-MacBook-Pro.local>
2018-04-13 06:23:07 -0400

    added stop-EC2 lambda code(boto3)

---
+++ EC2 /stop-ec2-instance-with-lambda-and-cloudwatch.py
@@ -0,0 +1,18 @@
+import boto3
+
+client=boto3.client('ec2')
+
+
+
+response=client.describe_instances()
+
+
+for reservation in response["Reservations"]:
+
+	for instance in reservation["Instances"]:
+
+        	print(instance["InstanceId"] + "stopping")
+
+        	id=[instance["InstanceId"]]
+
+        	client.stop_instances(InstanceIds=id)
